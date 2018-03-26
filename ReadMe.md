242a534c2fb7dce68abebb82ce0849a0802537ad
santhosh.kumar <santhosh.jayaraman88@gmail.com>
2018-03-26 17:01:07 +0530

    Added readme and other changes

---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+Chat Application using Socket.io and Mongo db written in ES6 way.
+
+Key points while setting up:
+If you are using mongo first time, please do the below steps.
+1. install mongo from their website.
+2. Create a folder /data/db in C: (Mongo works only in this folder)
+3. Go to mongo bin located inside Programfiles and open command prompt and run "mongod"
+4. The above step will start mongodb
+5. Open separate command window inside mongo bin and run " mongo"
+
+
+Some of the commands for mongo which you need to see the data.
+1. show dbs (shows all databases)
+2. db (shows which database you are in)
+3. use xxx (xxx refers to the database you want to navigate to)
+4. show collections (show collections inside the database)
+5. db.xxx.find() (To show the data inside the collection, replace xxx with the collections)
+
+
+To run the application
+1. install npm
+2. Also install nodemon globally "npm install nodemon -g"
+3. run npm start in cmd
+4. Browse the html page in firefox or host it in IIS or other web server using parcel/webpack/express. Running file directly in chrome will mess up with connecting socket and it won't work
+
+Inspired from Brad traversy tutorial.
+
+Future plans:
+1. Show list of connected users.
+2. Send screenshot using html2canvas across page.
--- index.js
+++ index.js
@@ -3,14 +3,77 @@ import http from 'http';
 import SocketIO from 'socket.io';
 import mongo from 'mongodb';
 
-let app=express();
-let server=http.Server(app);
-let io=new SocketIO(server);
+let app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
 io.liste
-let port=process.env.PORT||3000;
-let users=[];
-let mongoClient=mongo.MongoClient;
+let port = process.env.PORT || 3000;
+let users = [];
+let mongoClient = mongo.MongoClient;
 
-http.listen(port, function () {
-    console.log('listening on *:'+port);
+server.listen(port, function () {
+    console.log('listening on *:' + port);
 });
+mongoClient.connect('mongodb://127.0.0.1/mongochat', function (err, database) {
+    if (err) {
+        throw err;
+    }
+    console.log('Mongo db started');
+    let db = database.db('chatroomApp');
+    io.origins('*:*');
+    io.on('connection', (socket) => {
+        console.log("User connected");
+        let chat = db.collection('chats');
+
+        let sendStatus = (s) => {
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) {
+                throw err;
+            }
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+
+        // Handle input events
+        socket.on('input', function (data) {
+            let name = data.name;
+            let message = data.message;
+
+            // Check for name and message
+            if (name == '' || message == '') {
+                // Send error status
+                sendStatus('Please enter a name and message');
+            } else {
+                // Insert message
+                chat.insert({ name: name, message: message }, function () {
+                    io.emit('output', [data]);
+                    console.log(data);
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+        });
+
+        socket.on('clear', function (data) {
+            // Remove all chats from collection
+            chat.remove({}, function () {
+                // Emit cleared
+                io.emit('cleared');
+            });
+        });
+
+        socket.on('disconnect', () => {
+            console.log("User disconnected");
+        });
+
+    })
+});
+
+
